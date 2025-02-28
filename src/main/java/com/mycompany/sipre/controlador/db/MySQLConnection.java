be8111eb5c87ed821442e3c4db9a6b8d5125ab29
AngelSanchezMedina <96074341+AngelSanchezMedina@users.noreply.github.com>
2025-02-27 23:37:44 -0700

    BD Connection

--- src/main/java/com/mycompany/sipre/controlador/db/MySQLConnection.java
+++ src/main/java/com/mycompany/sipre/controlador/db/MySQLConnection.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/project";
+    private static final String URL = "jdbc:mysql://localhost:3306/sipre";
     private static final String USER = "root";
-    private static final String PASSWORD = "alexander14.";
+    private static final String PASSWORD = "hola.";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
